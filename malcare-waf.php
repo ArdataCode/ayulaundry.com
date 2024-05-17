87075deeeb0ac0eb9ee3238f0fe4a18e3f8253d3
ArdataCode <ardata.indonesia@gmail.com>
2024-05-17 15:19:02 +0700

    mit

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/ujianori/ayulaundry.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/ujianori/ayulaundry.com/wp-content/mc_data/');
+	define("MCCONFKEY", '6f1f62712aa511447547f4dba8f09c27');
+	include_once('/home/ujianori/ayulaundry.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'E7rgVErzVmJrnzqIHEiPHpibGhpBn5dGf5ETiTTx99fziNpAJwWFM3Y06yd8aKUS');
+define('SECURE_AUTH_KEY',  '2vwILFefILpcVeqKUBxVIEXVJJTYjfQVoVRmxYTfjcXmb9YRCptwAQNare1Y0KVM');
+define('LOGGED_IN_KEY',    '4fG7VGoaDWFdcgXoNr6qJrnP3yzz31LGX2o2eCrwobMXHn6ieu9UFRQqprHAb1BY');
+define('NONCE_KEY',        'JA0AVd2ehsKGCBiHceDNbs2em9YIvGWqgqjKtRgSf5LnK77mc001LRqiLarRR1RB');
+define('AUTH_SALT',        '2H2YMMFSyf5gR5dSrszwSPhorLDPGScNeMhohPu9wCLL7EWYwtcF9QX6G2Pndas6');
+define('SECURE_AUTH_SALT', 'pawgf79e7PSVqnyUfW0Nx0I5TRatf68wxoNvsiPqWdd2nm1hFFT1nD1WFy9Rp20r');
+define('LOGGED_IN_SALT',   'oR8g6t6YadXL0zKTVHX0S7BhngAmsdDVwS1AwVNcGF1MjYuavJ1VzG0o5xmGIdQi');
+define('NONCE_SALT',       '5P2otbz6ghwr0vT8KStWGAyA6QSEzMDtHT6sgYnEFvIfWPwFcosfQ6agXHjNUJnu');
